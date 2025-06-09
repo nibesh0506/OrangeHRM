b643e92dacc0aa1b5adf6ef5dc5bd9d0e8a82b71
nibesh0506 <156560414+nibesh0506@users.noreply.github.com>
2025-06-09 13:24:07 +0545

    Update dbtest.cy.js

--- cypress/e2e/dbtest.cy.js
+++ cypress/e2e/dbtest.cy.js
@@ -1,6 +1,6 @@
 describe("Bank DB Test", () => {
-    //  Note: In this it is getting a SQL Injection attack and it is vulnerable
-    it("Checks bank details", () => {
+    //  SQL Injection attack, and it is vulnerable
+     it("Checks bank details", () => {
         const maliciousInput = `' OR '1'='1`;
         cy.task(
             'queryDb',
@@ -10,8 +10,11 @@ describe("Bank DB Test", () => {
         ).then((result) => {
             expect(result).to.have.length(0);
         });
-    // UP TO HERE
-        const maliciousBankName = `XYZ BANK'); DROP TABLE bank; --`;
+        //  Up to here
+
+        //  Prevented from an SQL injection attack
+        const maliciousBankName = `XYZ BANK');
+         DROP TABLE atm_card; --`;
         cy.task(
             'queryDb',
             `INSERT INTO bank(bank_code, address, bank_name)
@@ -19,6 +22,36 @@ describe("Bank DB Test", () => {
         ).then((result) => {
             expect(result.error).to.exist;
         });
+        // Up to here
+
+        // ATM another SQL INJECTION
+        cy.task('queryDb',
+            `SELECT *
+             FROM bank.atm_card
+             WHERE cardNo = '${maliciousInput}'`
+        ).then((result) => {
+            expect(result).to.have.length(0);
+        })
+        cy.task(
+            'queryDb',
+            "INSERT INTO account(accNo) VALUES (1013);"
+        ).then((result) => {
+            expect(result.affectedRows).to.equal(1)
+        })
+
+        cy.task(
+            'queryDb',
+            "INSERT INTO atm_card(cardNo, expDate, CVV_no, accNo) VALUES (111,'2025-09-21',101,1013)"
+        ).then((result) => {
+            expect(result.affectedRows).to.equal(1);
+        });
+
+        cy.task(
+            'queryDb',
+            "DELETE FROM account where accNo IN (1013)"
+        ).then((result)=>{
+            expect(result.error).to.not.exist
+        })
 
     });
 });
